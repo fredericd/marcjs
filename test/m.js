b561c82133daf60a1bbc7ffca9079410e84a9a51
Frédéric Demians <f.demians@tamil.fr>
2013-12-05 21:56:09 +0100

    Add a MARC-in-JSON reader

--- test/m.js
+++ test/m.js
@@ -5,13 +5,14 @@ var marc = require('../lib/marcjs'),
 
 var reader = new marc.MarcxmlReader(fs.createReadStream('test/data/bib.xml'));
 //var writer = new marc.JsonWriter(fs.createWriteStream('test/data/bib-out.xml'));
-var writer = new marc.MiJWriter(fs.createWriteStream('test/data/bib-out.json'));
+var writer = new marc.MiJWriter(fs.createWriteStream('test/data/bib-out.mij'));
 
 reader.on('data', function(record) {
     record.append([ '999', '  ', 'a', 'Demians', 'b', 'Frédéric'])
           .append([ '206', '  ', 'a', 'Demians Pauline']);
-    console.log(record.as('text'));
     writer.write(record);
+    console.log('RECORD #' + writer.count);
+    console.log(record.as('mij'));
 });
 var intervalId = setInterval(function() {
     console.log(reader.count);
