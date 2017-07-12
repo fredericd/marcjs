b4dc6ed1c9a1a1b28909b627cbeaf325f533f1c8
Jim Nicholls <jim.nicholls@sydney.edu.au>
2017-07-12 14:38:26 +1000

    Implement incremental reading for Iso2709Reader.
    Change Iso2709Reader to emit a data event as soon as it has received enough data to parse a record.
    
    Fixes issue #19.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,9 @@
 
 var should = require('should'),
     fs     = require('fs'),
-    m      = require('../lib/marcjs.js');
+    m      = require('../lib/marcjs.js'),
+    async  = require('async'),
+    stream = require('stream');
 
 describe('Record', function() {
   var record = new m.Record();
@@ -50,6 +52,298 @@ describe('Issue #16', function() {
     reader.on('end', function () { done(); });
   });
 });
+
+describe('Iso2709ReadStream incremental reading', function () {
+
+  // Array of objects: each object has a iso2709 property being a buffer with a ISO 2709 record,
+  //                   and a leader property being the leader extracted from from the ISO 2709 record.
+  let records = [];
+
+  before(function () {
+    // Read the records in test/data/bib.mrc into the marcRecords array, one record per array element.
+    let buf = fs.readFileSync('test/data/bib.mrc');
+    for (let recordStart = 0, recordEnd = 0; recordStart < buf.length; recordStart = recordEnd + 1) {
+      recordEnd = buf.indexOf(0x1D, recordStart);
+      if (recordEnd === -1) {
+        // Ignore trailing bytes after the last record terminator.
+        break;
+      }
+      let iso2709 = buf.slice(recordStart, recordEnd + 1);
+      let leader = iso2709.slice(0, 24).toString('ascii');
+      let expectedLength = Number.parseInt(leader.slice(0, 5));
+      iso2709.should.have.lengthOf(expectedLength);
+      records.push({iso2709, leader});
+    }
+    // Confirm we read in all 6 records
+    records.should.be.lengthOf(6);
+  });
+
+  it('emits a data event after each record', function(done) {
+    let dummyInput = new stream.PassThrough();
+    let reader = new m.Iso2709Reader(dummyInput);
+    let curRecIdx = -1;
+    let curRec;
+
+    let timeout;
+
+    function pumpNextRecord() {
+      if (typeof timeout !== 'undefined') {
+        clearTimeout(timeout);
+      }
+      curRecIdx++;
+      curRec = records[curRecIdx];
+      if (typeof curRec !== 'undefined') {
+        if (curRecIdx === records.length - 1) {
+          dummyInput.end(curRec.iso2709);
+        } else {
+          dummyInput.write(curRec.iso2709);
+        }
+        timeout =
+          setTimeout(function() {
+            done(new Error(`Iso2709Reader did not emit a data event after pumping ${curRecIdx + 1} records into the input stream`));
+          }, 1000);
+      }
+    }
+
+    reader.on('data', function(parsedRecord) {
+      try {
+        parsedRecord.leader.should.equal(curRec.leader);
+        pumpNextRecord();
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    reader.on('end', function() {
+      if (typeof timeout !== 'undefined') {
+        clearTimeout(timeout);
+      }
+      try {
+        done(null);
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    pumpNextRecord();
+
+  }); // it 'emits a data event after each record'
+
+  it("doesn't emit a data event until a whole record has been received", function(done) {
+    let dummyInput = new stream.PassThrough();
+    let reader = new m.Iso2709Reader(dummyInput);
+
+    let dataEventCount = 0;
+    let timeout;
+
+    function pumpChunksOfRecord(recordIndex = 0, numberOfChunks = 3) {
+      const iso2709    = records[recordIndex].iso2709;
+      const recordSize = iso2709.length;
+      const chunkSize  = Math.floor(recordSize / numberOfChunks);
+      function pumpNextChunk(startByte) {
+        if (startByte < recordSize) {
+          setImmediate(function() {
+            dummyInput.write(iso2709.slice(startByte, startByte + chunkSize));
+            pumpNextChunk(startByte + chunkSize);
+          })
+        } else {
+          dummyInput.end();
+        }
+      }
+      pumpNextChunk(0);
+    }
+
+    reader.on('data', function(parsedRecord) {
+      try {
+        dataEventCount++;
+        parsedRecord.leader.should.equal(records[0].leader);
+        dummyInput.end();
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    reader.on('end', function() {
+      if (typeof timeout !== 'undefined') {
+        clearTimeout(timeout);
+      }
+      try {
+        dataEventCount.should.equal(1);
+        done(null);
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    timeout =
+      setTimeout(function() {
+        done(new Error('Iso2709Reader did not emit a data event after pumping all the chunk of the record'));
+      }, 1000);
+
+    pumpChunksOfRecord()
+
+  });
+
+  it('emits multiple data events when multiple records are received at once', function(done) {
+    let dummyInput = new stream.PassThrough();
+    let reader = new m.Iso2709Reader(dummyInput);
+    let curRecIdx = 0;
+
+    let timeout;
+
+    reader.on('data', function(parsedRecord) {
+      try {
+        parsedRecord.leader.should.equal(records[curRecIdx].leader);
+        curRecIdx++;
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    reader.on('end', function() {
+      if (typeof timeout !== 'undefined') {
+        clearTimeout(timeout);
+      }
+      try {
+        curRecIdx.should.equal(records.length);
+        done(null);
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    dummyInput.write(Buffer.concat(records.map(_1 => _1.iso2709)));
+    dummyInput.end();
+
+    timeout =
+      setTimeout(function() {
+        done(new Error('Iso2709Reader did not emit all the data event after pumping all the records at once'));
+      }, 1000);
+  });
+
+  it('handles records starting or ending in the middle of a chunk', function(done) {
+    let dummyInput = new stream.PassThrough();
+    let reader = new m.Iso2709Reader(dummyInput);
+    let curRecIdx = 0;
+
+    let halfRecord1Index = Math.floor(records[1].iso2709.length / 2);
+    let halfRecord2 = records[2].iso2709.slice(0, Math.floor(records[2].iso2709.length / 2));
+    let chunks = [
+      Buffer.concat([records[0].iso2709, records[1].iso2709.slice(0, halfRecord1Index)]),
+      Buffer.concat([records[1].iso2709.slice(halfRecord1Index), halfRecord2]),
+    ];
+    let chunkIndex = 0;
+
+    let timeout;
+
+    function pumpNextChunk() {
+      if (chunkIndex < chunks.length) {
+        dummyInput.write(chunks[chunkIndex]);
+        chunkIndex++;
+        setImmediate(pumpNextChunk);
+      } else {
+        dummyInput.end();
+      }
+    }
+
+    reader.on('data', function(parsedRecord) {
+      try {
+        parsedRecord.leader.should.equal(records[curRecIdx].leader);
+        curRecIdx++;
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    reader.on('end', function() {
+      if (typeof timeout !== 'undefined') {
+        clearTimeout(timeout);
+      }
+      try {
+        curRecIdx.should.equal(2);
+        reader.data.should.eql([halfRecord2], 'the incomplete half of record #2 should remain in the reader');
+        done(null);
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    pumpNextChunk();
+
+    timeout =
+      setTimeout(function() {
+        done(new Error('Iso2709Reader did not emit all the data events after pumping all the chunks'));
+      }, 1000);
+
+  });
+
+  it("doesn't block nextTick", function(done) {
+    let dummyInput = new stream.PassThrough();
+    let reader = new m.Iso2709Reader(dummyInput);
+
+    let loopCount = 0;
+
+    let timeout;
+
+    let dataCount = 0;
+    let intervalCount = 0;
+    let interval = setInterval(function() { intervalCount++ }, 1);
+
+    function pumpRecords() {
+      if (intervalCount === 0) {
+        dummyInput.write(Buffer.concat(records.map(_1 => _1.iso2709)));
+        loopCount++;
+      } else {
+        dummyInput.end();
+      }
+    }
+
+    reader.on('data', function() {
+      try {
+        dataCount++;
+        if (dataCount % records.length === 0) {
+          setImmediate(pumpRecords);
+        }
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    reader.on('end', function() {
+      if (typeof timeout !== 'undefined') {
+        clearTimeout(timeout);
+      }
+      if (typeof interval !== 'undefined') {
+        clearTimeout(interval);
+      }
+      try {
+        intervalCount.should.be.above(0);
+        dataCount.should.equal(records.length * loopCount);
+        done(null);
+      }
+      catch (err) {
+        done(err);
+      }
+    });
+
+    pumpRecords();
+
+    timeout =
+      setTimeout(function() {
+        done(new Error('Iso2709Reader is blocking nextTick'));
+      }, 1000);
+  });
+
+}); // describe 'Iso2709ReadStream incremental reading'
 
 describe('MiJWriter', function () {
   var reader, record;
