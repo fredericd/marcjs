1db42daede238679e4794dc78c875f6a1026e831
Frédéric Demians <f.demians@tamil.fr>
2025-02-25 18:52:10 +0100

    ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
